4ab7fdffb71902f3b88fb621f8672da6615b932f
sultanalsubki <146655214+sultanalsubki@users.noreply.github.com>
2025-01-29 19:10:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -431,6 +431,6 @@ def signin():
             close_connection(connection)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render’s assigned port or default to 5000
+    port = int(os.environ.get("PORT", 10000))  # Use Render’s assigned port or default to 5000
     app.run(host="0.0.0.0", port=port)  # Ensure the app binds to 0.0.0.0
 
