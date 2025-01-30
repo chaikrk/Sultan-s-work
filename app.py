7bef14b5df3def28cfe5c8ebbdd4e79524fc3e01
sultanalsubki <146655214+sultanalsubki@users.noreply.github.com>
2025-01-30 18:43:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -459,3 +459,10 @@ def add_skill():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+    #--------------------------------------------------------------------------
+#try for chat
+#--------------------------------------------------------------------------
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
